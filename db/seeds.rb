850c63882befe25b745003c44e22884a7a85f3a1
IBUNHABIBU <ibunhabib@gmail.com>
2022-10-11 22:47:32 +0300

    Seed the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,9 @@
 
 100.times do 
     Post.find_or_create_by(title: Faker::Quote.famous_last_words) do |post|
-        post.content = Faker::Lorem.paragraphs(
-            number: 5,
-            supplemental: true
+        post.content = Faker::Lorem.paragraph(
+            sentence_count: 45,
+            supplemental: true,
             random_sentences_to_add: 4
             )
     end
