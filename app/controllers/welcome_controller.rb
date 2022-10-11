c06acc46fc1301a6cf939e96d9fdbd04ded4ce64
IBUNHABIBU <ibunhabib@gmail.com>
2022-10-12 00:25:17 +0300

    Add result partial

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,12 @@
 class WelcomeController < ApplicationController
   def index
+    @posts = params[:query] ? Post.where("title LIKE ?", "%#{params[:query]}%") : []
+
+    respond_to do |format|
+      format.html {}
+      format.turbo_stream {
+        render turbo_stream: turbo_stream.replace("posts", partial: "welcome/results")
+      }
+    end
   end
 end
