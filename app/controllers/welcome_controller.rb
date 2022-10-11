6c39956832daf2c5335f750e8a78469e9b4d9ceb
IBUNHABIBU <ibunhabib@gmail.com>
2022-10-12 00:34:52 +0300

    Add result partial

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,8 +5,13 @@ class WelcomeController < ApplicationController
     respond_to do |format|
       format.html {}
       format.turbo_stream {
-        render turbo_stream: turbo_stream.replace("posts", partial: "welcome/results")
+        render turbo_stream: turbo_stream.replace("results", partial: "welcome/results")
       }
     end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    render turbo_stream: turbo_stream.replace("show_content", partial: "welcome/show")
   end
 end
