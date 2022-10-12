21ebb9fa70f74061a11a439edf295df065aee7ca
IBUNHABIBU <ibunhabib@gmail.com>
2022-10-12 18:51:24 +0300

    Add Quotes controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   resources :welcome, only: [:show]
+  resources :quotes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
